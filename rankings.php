5971bd78687b7660ba44d6b9621f3d16154cc29f
BuildTools <unconfigured@null.spigotmc.org>
2020-08-23 16:12:13 +0200

    Rankings added first test commit

--- rankings.php
+++ rankings.php
@@ -1,57 +1,110 @@
-<!DOCTYPE HTML>
-<html>
-<head>  
-	 <meta name="viewport" content="width=device-width, initial-scale=1">
-	  <!-- CSS  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Starter Template - Materialize</title>
+
+  <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-	<style>
-	
-	#messages > div {
-		font-size: 17px;
-		font-family: cursive;
-	}
-	.title_map  {
-		display: grid;
-		background: red;
-	}
-	.canvas_container {
-		height: 300px;
-		margin: 20px;
-	}
-	.canvas_container > div {
-		 position: relative;
- height: 300px;
-	}
-	.canvas_container > div > canvas {
-		 position: absolute;
- width: 100%;
- height: 300px;
-	}
-	
-	</style>
-<script>
-window.onload = function () {
-}
-</script>
+     
+     
 </head>
 <body>
-	<div class="section blue lighten-5" id="index-banner">
+  <div class="section blue lighten-5" id="index-banner">
     <div class="container">
-      <h1 class="header center blue-text">Map Statistics</h1>
-      <div class="row center">  
-      </div>
-
+      <h1 class="header center blue-text">Map Rankings </h1>
     </div>
   </div>
-  
-<div id="messages" class="blue lighten-5" ></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</body>
+
+
+  <div class="blue lighten-5">
+    <div class="section">
+      <div class="row">
+      
+        <div class="col xl3 l4 m6 s12">
+          <div class="icon-block">
+            
+            <h5 class="center">
+              <span class="center light-blue-text">
+                <i class="material-icons">trending_up</i>
+              </span>
+              <span>Jailbreak</span>
+            </h5>
+            
+              <ul class="collection" id="ranking_jailbreak">
+              </ul>
+              
+          </div>
+        </div>      
+      
+        <div class="col xl3 l4 m6 s12">
+          <div class="icon-block">
+          
+            <h5 class="center">
+              <span class="center light-blue-text">
+                <i class="material-icons">trending_up</i>
+              </span>
+              <span>Top Traitor</span>
+            </h5>          
+
+            <ul class="collection" id="most_killed_ranking">
+            </ul>
+            
+              </div>
+        </div>
+
+        <div class="col xl3 l4 m6 s12">
+          <div class="icon-block">
+            <h5 class="center">
+              <span class="center light-blue-text">
+                <i class="material-icons">search</i>
+              </span>
+              <span>Scanned bodies</span>
+            </h5>      
+            
+            <ul class="collection" id="identified_bodies_ranking">
+            </ul>
+
+          </div>
+        </div>
+        
+        <div class="col xl3 l4 m6 s12">
+          <div class="icon-block">
+            <h5 class="center">
+              <span class="center light-blue-text">
+                <i class="material-icons">local_grocery_store</i>
+              </span>
+              <span>Shopping Queen</span>
+            </h5>  
+            
+             <ul class="collection" id="shopping_queen_ranking">
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+  </div>
+
+  <footer class="page-footer blue">
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="https://steamcommunity.com/id/IAmFat__/" target="_blank">IAmFat</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  </body>
 </html>
 <?php
 
@@ -77,8 +130,8 @@ $request_states_sixty = $conn->query($cmd_request_state_sixty_minutes);
 foreach($request_states_sixty->fetch_all() as $requests_info){
  // print_r($value);// "Salary: $value<br>";
   echo "<div style='color:green;'>ShortName: ". $requests_info[0]." MapDisplayName: ". $requests_info[1];
-  $playtime = round(floor((int)$requests_info[2])/ 60, 2);
-  echo "Playtime : <b>".$playtime."%</b></div>";
+  $playtime = round(floor((int)$requests_info[2])/ 60, 0);
+  echo "Playtime : <b>".$playtime." Hours</b></div>";
   
 }
 ?>
