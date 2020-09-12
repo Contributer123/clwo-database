57df3d93b044eea5cc6eeffe00922e2542b0e44a
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 11:47:50 +0200

    testing git pull script

--- pull.php
+++ pull.php
@@ -1,7 +1,9 @@
 <?php
+  echo "Testing Pull Script:"
   echo "<pre>";
   $result = array();
   chdir('/var/www/html');
+  echo "Path after changing dir: " .getcwd();
   $output = shell_exec('./shell-script.sh');
   echo $output;
   echo "<pre>";
