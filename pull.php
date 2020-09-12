c1d4a1fd88df4588a0af10503f46df81925de227
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 12:00:48 +0200

    testing git pull script

--- pull.php
+++ pull.php
@@ -4,7 +4,7 @@
   $result = array();
   chdir('/var/www/html');
   echo "Path after changing dir: " .getcwd();
-  $output = shell_exec('/var/www/html/pull_script.sh');
+  $output = shell_exec('/home/pi/Desktop/pull_script.sh');
   echo $output;
   $output = exec('cat "helloe"');
   echo "here.sth.".$output;
