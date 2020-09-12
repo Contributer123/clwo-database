b60121e19f4886a063734cbca0532316febf4dc2
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 12:01:37 +0200

    testing git pull script

--- pull.php
+++ pull.php
@@ -1,4 +1,18 @@
 <?php
+  
+  
+  $dir = "/home/pi/Desktop/";
+
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+    while (($file = readdir($dh)) !== false){
+      echo "filename:" . $file . "<br>";
+    }
+    closedir($dh);
+  }
+}
+
   echo "Testing Pull Script:";
   echo "<pre>";
   $result = array();
