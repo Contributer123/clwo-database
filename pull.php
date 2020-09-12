da5010c695923b8aa5e690033fadae503fc2a701
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 11:46:17 +0200

    testing git pull script

--- pull.php
+++ pull.php
@@ -1,13 +1,9 @@
 <?php
-function execPrint($command) {
-    $result = array();
-    exec($command, $result);
-    foreach ($result as $line) {
-        print($line . "\n");
-    }
-}
-// Print the exec output inside of a pre element
-print("<pre>" . execPrint("git pull") . "</pre>");
+  echo "<pre>";
+  $result = array();
+  chdir('/var/www/html');
+  $output = shell_exec('./shell-script.sh');
+  echo $output;
+  echo "<pre>";
 ?>
-  
-  
+
